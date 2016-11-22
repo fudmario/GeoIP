6ff02aea2d3c1510dbf8548ebe9e021c094fc391
fudmario <fudmariophp@gmail.com>
2016-11-22 17:42:19 -0400

    Add Flags

--- GeoIp/GeoIp/Form1.Designer.vb
+++ GeoIp/GeoIp/Form1.Designer.vb
@@ -22,17 +22,105 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ListView1 = New System.Windows.Forms.ListView()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtIp = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.imgList = New System.Windows.Forms.ImageList(Me.components)
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'ListView1
+        '
+        Me.ListView1.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ListView1.FullRowSelect = True
+        Me.ListView1.GridLines = True
+        Me.ListView1.Location = New System.Drawing.Point(0, 58)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(373, 317)
+        Me.ListView1.SmallImageList = Me.imgList
+        Me.ListView1.TabIndex = 2
+        Me.ListView1.UseCompatibleStateImageBehavior = False
+        Me.ListView1.View = System.Windows.Forms.View.Details
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtIp)
+        Me.GroupBox1.Controls.Add(Me.btnAdd)
+        Me.GroupBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(373, 58)
+        Me.GroupBox1.TabIndex = 3
+        Me.GroupBox1.TabStop = False
+        '
+        'txtIp
+        '
+        Me.txtIp.Location = New System.Drawing.Point(84, 20)
+        Me.txtIp.Name = "txtIp"
+        Me.txtIp.Size = New System.Drawing.Size(277, 20)
+        Me.txtIp.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Image = CType(resources.GetObject("btnAdd.Image"), System.Drawing.Image)
+        Me.btnAdd.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft
+        Me.btnAdd.Location = New System.Drawing.Point(12, 20)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(66, 20)
+        Me.btnAdd.TabIndex = 2
+        Me.btnAdd.Text = "Add IP"
+        Me.btnAdd.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Name"
+        Me.ColumnHeader1.Width = 120
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Code"
+        Me.ColumnHeader2.Width = 100
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "GeoNameId"
+        Me.ColumnHeader3.Width = 100
+        '
+        'imgList
+        '
+        Me.imgList.ColorDepth = System.Windows.Forms.ColorDepth.Depth32Bit
+        Me.imgList.ImageSize = New System.Drawing.Size(16, 16)
+        Me.imgList.TransparentColor = System.Drawing.Color.Transparent
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(643, 375)
+        Me.ClientSize = New System.Drawing.Size(373, 375)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.ListView1)
         Me.Name = "Form1"
         Me.Text = "GeoIP and Flags"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents ListView1 As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtIp As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents imgList As ImageList
 End Class
--- GeoIp/GeoIp/Form1.vb
+++ GeoIp/GeoIp/Form1.vb
@@ -1,3 +1,27 @@
-﻿Public Class Form1
+﻿Imports System.Net
+Imports MaxMind.GeoIP2
+Imports MaxMind.GeoIP2.Responses
 
+Public Class Form1
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim ipValue As String = txtIp.Text
+        Dim flags As String = Application.StartupPath & "\flags"
+        If IsValidIP(ipValue) Then
+            Using dbRead As New DatabaseReader(Application.StartupPath & "\GeoLite2-Country.mmdb")
+                Dim cr As CountryResponse = dbRead.Country(ipValue)
+                Dim img As Image = Image.FromFile($"{flags}\{cr.Country.IsoCode.ToLower}.png")
+                imgList.Images.Add(cr.Country.IsoCode, img)
+                Dim lv As New ListViewItem({cr.Country.Name, cr.Country.IsoCode, cr.Country.GeoNameId})
+                lv.ImageKey = cr.Country.IsoCode
+                ListView1.Items.Add(lv)
+            End Using
+        Else
+            MessageBox.Show("Invalid IP")
+
+
+        End If
+    End Sub
+    Private Function IsValidIP(address As String) As Boolean
+        Return IPAddress.TryParse(address, New IPAddress(0))
+    End Function
 End Class
